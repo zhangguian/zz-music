5b0017c79ccbd8ea184979631bf5b152e29ff35d
zhangguian <2027432474@qq.com>
2022-04-29 22:08:29 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangguian
+ * @description defaultController
+ * @date 2022/4/29
+ */
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+    @GetMapping
+    public String sayHello() {
+        return "欢迎程序员ANJING的音乐盒";
+    }
+}
