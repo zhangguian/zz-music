88824be8d1def2c451f29200745aed2026d2694e
zhangguian <2027432474@qq.com>
2022-05-04 11:50:43 +0800

    add cors

--- src/main/java/com/example/demo/controller/DefaultController.java
+++ src/main/java/com/example/demo/controller/DefaultController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin
 public class DefaultController {
     @GetMapping
     public String sayHello() {
